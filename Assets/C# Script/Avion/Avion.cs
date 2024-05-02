5f539e5d80b626da2a35a09f25afdc1830dd4819
Arkaitz <127892239+ArkaitzL@users.noreply.github.com>
2024-05-02 14:08:13 +0200

    Paquetes

---
+++ Assets/C# Script/Avion/Avion.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BaboOnLite;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Avion : MonoBehaviour
+{
+    Controlador.Teclas.Avion teclas {
+        get => controlador.teclas.avion;
+    }
+
+    [Header("Velocidades")]
+    [SerializeField] float aceleracion = .1f;
+    [SerializeField] float aceleracion_max = 100;
+    [SerializeField] float velocidad = 200;
+    [Header("Friccion")]
+    [SerializeField] float friccion_aire = 0.01f;
+    [SerializeField] float friccion_suelo = 1f;
+    [Header("Manejabiliad")]
+    [SerializeField] float manejabilidad_alabeo = 20;
+    [SerializeField] float manejabilidad_cabeceo = 20;
+    [SerializeField] float manejabilidad_guinada = 20;
+    [Header("Otras fuerzas")]
+    [SerializeField] float fuerza_elevacion = 135;
+
+    [Header("Componentes")]
+    [SerializeField] Transform helice;
+    [SerializeField] GameObject paquete;
+    [SerializeField] Transform deposito_posicion;
+
+    float potencia, planeo;             //Fuerzas   
+    float alabeo, cabeceo, guinada;     //Controles
+    bool enSuelo;
+
+    Controlador controlador;
+    HUD hud;
+    Rigidbody rb;
+    Animator animacion;
+
+    void Start() 
+    {
+        controlador = Instanciar<Controlador>.Coger();
+        hud = Instanciar<HUD>.Coger();
+
+        rb = GetComponent<Rigidbody>();
+        animacion = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Controles();
+        Helice();
+
+        hud.Info1(
+            potencia,
+            rb.velocity.magnitude * 3.6f,
+            //rb.velocity.z * 3.6f,
+            transform.position
+       );
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * velocidad * planeo);         //Acelerar
+
+        rb.AddTorque(transform.up * guinada * fuerzas(manejabilidad_guinada));      //Rotar
+        rb.AddTorque(transform.right * cabeceo * fuerzas(manejabilidad_cabeceo));   //Ascender
+        rb.AddTorque(transform.forward * alabeo * fuerzas(manejabilidad_alabeo));   //Inclinarse
+
+        rb.AddForce(Vector3.up * rb.velocity.magnitude * fuerza_elevacion);         //Elevacion
+    }
+
+    float fuerzas(float manejabilidad) => (rb.mass / 10f) * manejabilidad;
+
+    void Controles() 
+    {
+        //Control de avion
+        alabeo = Input.GetKey(teclas.alabeo_positivo) ? 1 : (Input.GetKey(teclas.alabeo_negativo) ? -1 : 0);
+        cabeceo = Input.GetKey(teclas.cabeceo_positivo) ? 1 : (Input.GetKey(teclas.cabeceo_negativo) ? -1 : 0);
+        guinada = Input.GetKey(teclas.guinada_positivo) ? 1 : (Input.GetKey(teclas.guinada_negativo) ? -1 : 0);
+
+        //alabeo = Input.GetAxis("Alabeo");
+        //cabeceo = Input.GetAxis("Cabeceo");
+        //guinada = Input.GetAxis("Guinada");
+
+        //Aceleracion
+        Acelerar();
+
+        //Soltar paquete
+        if (Input.GetKeyDown(teclas.soltar_paquete) && ControladorBG.Esperando("Deposito")) {
+            ControladorBG.IniciarEspera("Deposito", 2f);
+            SoltarPaquete();
+        } 
+    }
+
+    void Acelerar() 
+    {
+        if (Input.GetKey(teclas.acelerar)) {
+            potencia += aceleracion;
+        }
+        if (Input.GetKey(teclas.desacelerar)) {
+            potencia -= aceleracion;
+        }
+
+        potencia = Mathf.Clamp(potencia, 0, aceleracion_max);
+
+        //Friccion
+        if (planeo < potencia) planeo = potencia;               
+        if (planeo > potencia && planeo >= 0) planeo -= (enSuelo) ? friccion_suelo : friccion_aire;
+    }
+
+    void SoltarPaquete() 
+    {
+        animacion.SetTrigger("Deposito");
+        ControladorBG.Rutina(.75f, () => {      
+            GameObject inst = Instantiate(paquete, deposito_posicion.position, Quaternion.identity);
+            inst.GetComponent<Rigidbody>().velocity = rb.velocity;
+        });
+    }
+
+    void Helice() 
+    {
+        if (helice == null) return;
+        helice.Rotate(-Vector3.forward * (potencia / 5));
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!enSuelo)
+        {
+            hud.Info2(true);
+            enSuelo = true;
+
+            GameObject parte = collision.GetContact(0).thisCollider.gameObject;
+            if (parte.CompareTag("Choque"))
+            {
+                // ** TEMPORAL **
+                Debug.Log("Boom");
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        hud.Info2(false);
+        enSuelo = false;
+    }
+}
---
+++ Assets/C# Script/Controlador.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BaboOnLite;
+
+public class Controlador : MonoBehaviour
+{
+    [Serializable] public class Teclas {
+
+        public Avion avion;
+        public KeyCode cambio_cam = KeyCode.Tab;
+
+        [Serializable] public class Avion {    
+            public KeyCode acelerar = KeyCode.LeftShift;
+            public KeyCode desacelerar = KeyCode.LeftControl;
+            public KeyCode alabeo_negativo = KeyCode.D;
+            public KeyCode alabeo_positivo = KeyCode.A;
+            public KeyCode cabeceo_negativo = KeyCode.S;
+            public KeyCode cabeceo_positivo = KeyCode.W;
+            public KeyCode guinada_negativo = KeyCode.Q;
+            public KeyCode guinada_positivo = KeyCode.E;
+            public KeyCode soltar_paquete = KeyCode.Space;
+        }
+    }
+
+    [SerializeField] public Teclas teclas;
+
+    void Awake()
+    {
+        Instanciar<Controlador>.Añadir(this);
+    }
+
+}
